549437fda41a4e7d1c687b1cb5230a02369c67e0
Arredondo-Juan <juan_arredondo@icloud.com>
2024-07-25 23:10:05 -0500

    - style home view

--- DentalSpanish/Views/ContentView.swift
+++ DentalSpanish/Views/ContentView.swift
@@ -10,14 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            List {
-                ForEach(DeckType.allCases, id: \.self) { deckType in
-                    NavigationLink(destination: FlashcardListView(deckType: deckType)) {
-                        Text(deckType.rawValue)
+            ZStack {
+                LinearGradient(colors: [Color(.green), Color(.blue)],
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                
+                    VStack(spacing: 30) {
+                        ForEach(DeckType.allCases, id: \.self) { deckType in
+                            NavigationLink(destination: FlashcardListView(deckType: deckType)) {
+                                Text(deckType.rawValue)
+                            }
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                            .frame(width: 300, height: 180)
+                            .background(RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.blue)
+                                .shadow(radius: 5, y: 5)
+                                .opacity(0.6))
+                        }
                     }
-                }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.large)
             }
-            .navigationTitle("Dental Spanish")
         }
     }
 }
