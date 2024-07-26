25f15aab95371c7d6544ddc20b48a1e1652c7ee2
Arredondo-Juan <juan_arredondo@icloud.com>
2024-07-25 20:22:54 -0500

    Initial Commit

---
+++ DentalSpanish/DentalSpanishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentalSpanishApp.swift
+//  DentalSpanish
+//
+//  Created by Juan Arredondo on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DentalSpanishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
